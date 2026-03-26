f8d2e07813d1c243de1f7fd7cf16aee4f0258fb4
Bhushan Bhomkar <bhushanbhomkar20@gmail.com>
2026-03-26 16:21:18 +0530

    added rate limiter

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -3,8 +3,18 @@ const {registerUser,loginUser}= require('../controllers/userController');
 
 const router= express.Router();
 
+const rateLimit = require("express-rate-limit");
+
+const loginLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 5, 
+  message: {
+    success: false,
+    message: "Too many login attempts, try again later",
+  },
+});
 
 router.post('/register', registerUser);
-router.post('/login',loginUser);
+router.post("/login", loginLimiter, loginUser);
 
 module.exports = router;
--- Server.js
+++ Server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const dotenv = require('dotenv');
 const connectDB = require('./config/db')
 const {errorHandler}= require('./middleware/errorMiddleware');
-
+const rateLimit = require('express-rate-limit');
 
 dotenv.config();
 
@@ -17,6 +17,16 @@ console.log("connected to db");
 const app = express();
 app.use(express.json());
 
+const limiter=rateLimit({
+    windowMs:15*60*1000,
+    max:100,
+    message:{
+        success:false,
+        message:"Too many requests, try after some time",
+    },
+});
+
+app.use(limiter);
 
 app.use('/api/users', require('./Routes/userRoutes'));
 app.use('/api/books', require('./Routes/bookRoutes'));
